5554d1fcb4ecbcf6f846cdc03cdf8d1ca0d0cb99
TirthPatel7029 <156793271+TirthPatel7029@users.noreply.github.com>
2024-01-16 17:44:48 +0530

    Create intro.c

---
+++ intro.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+main()
+{
+		printf("name:-patel tirth \n");
+		printf("D.O.B:06-10-2005\n");
+		printf("height:5.10\n");
+		printf("father occupation:textile\n");
+		printf("Addresh:15,swapan shrusti ro-house\n");
+		printf("Father name:patel jayeshbhai\n");
+		printf("Contact Number:8758216569\n");
+		printf("thank you\n");
+}
